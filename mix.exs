5e00733bad71c73ff6700f1f147f8209ce371a9a
Aron Fyodor Asor <aronasorman@gmail.com>
2014-11-08 22:02:53 -0800

    add the Poison JSON parser

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Exchange.Mixfile do
   # Type `mix help deps` for examples and options
   defp deps do
     [{:phoenix, "0.5.0"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:poison, git: "https://github.com/devinus/poison.git"}]
   end
 end
